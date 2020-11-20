4d4d00ee353dc1e4dc192c93d82f2dd2a155326f
ose-me <oseeronlan@gmail.com>
2020-11-20 23:04:25 +0100

    feat:dropdown menus

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,4 @@
+// responsive navbar function
 function responsiveNav() {
   var x = document.getElementById("bottom-nav-mobile");
   if (x.className === "bottom-nav") {
@@ -6,3 +7,27 @@ function responsiveNav() {
     x.className = "bottom-nav";
   }
 }
+
+// toggle add repositories dropdown
+document
+  .getElementById("add-dropdown-btn")
+  .addEventListener("click", function () {
+    document.getElementById("dropdown-menu1").classList.toggle("show");
+  });
+
+// toggle profile details dropdown
+document
+  .getElementById("profile-dropdown-btn")
+  .addEventListener("click", function () {
+    document.getElementById("dropdown-menu2").classList.toggle("show");
+  });
+
+//scroll animation on repo header
+const headerUserNode = gitHubDOM.getNode(".repo_app_header-user");
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 370) {
+    headerUserNode.classList.remove("hide");
+  } else {
+    headerUserNode.classList.add("hide");
+  }
+});
